3147cb3ac1f3552f6fcfce12ad15eacb4c41a797
Analice <analice.leite12@gmail.com>
2024-10-06 09:55:06 -0300

    ✨ Feature url routes, views tests

---
+++ tests/views/test_post_view.py
@@ -0,0 +1,12 @@
+import pytest 
+import json
+import pdb
+from django.urls import reverse 
+
+@pytest.mark.django_db
+def test_post_view(client):
+    url = reverse('home')
+    response = client.get(url)
+    assert response.status_code == 200
+
+    assert response.content == b'Hello World'
